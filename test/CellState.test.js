5b71d48d15370792dce3d0da07337db8ab8956e7
Sergi <solive@uic.es>
2020-10-07 16:35:15 +0200

    dead and alive cell states added - tests green

---
+++ test/CellState.test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import CellState from '../src/CellState.js';
+
+describe('CellState', () => {
+    it('Should have a ALIVE State', () => {
+        expect(CellState.ALIVE).to.equal(1)
+    });
+
+    it('Should have a DEAD State', () => {
+        expect(CellState.DEAD).to.equal(0)
+    })
+})
