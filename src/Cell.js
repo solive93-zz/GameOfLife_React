39609e706c0c21d23f6c64f3e841449ab32d1361
Sergi <solive@uic.es>
2020-10-07 17:08:08 +0200

    a cell remains alive when has 2 or 3 alive neighbors - tests in green

--- src/Cell.js
+++ src/Cell.js
@@ -3,4 +3,11 @@ export default class Cell {
     constructor(state) {
         this.state = state;
     }
+
+    getNextState(numberOfAliveNeighbors) {
+        if(numberOfAliveNeighbors < 2) {
+            return 0
+        }
+        return 1;
+    }
 }
--- test/Cell.test.js
+++ test/Cell.test.js
@@ -13,4 +13,16 @@ describe('Cell', () => {
         const cell = new Cell(CellState.DEAD);
         expect(cell.state).to.equal(CellState.DEAD);
     })
+
+    it('Should die if has fewer than 2 alive neighbors', () => {
+        const cell = new Cell(CellState.ALIVE);
+        const nextState = cell.getNextState(1);
+        expect(nextState).to.equal(CellState.DEAD);
+    })
+
+    it('Should remain alive when has 2 or 3 alive neighbors', () => {
+        const cell = new Cell(CellState.ALIVE);
+        const nextState = cell.getNextState(2);
+        expect(nextState).to.equal(CellState.ALIVE);
+    })
 })
